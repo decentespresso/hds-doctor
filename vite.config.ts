1d9ff85415012591214cc3758bc745d49b04cdef
Vid Tadel <tadelv@gmail.com>
2026-03-21 20:21:24 +0100

    ci: add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
+  base: '/hds-doctor/',
   plugins: [mkcert()],
   server: {
     https: true,
